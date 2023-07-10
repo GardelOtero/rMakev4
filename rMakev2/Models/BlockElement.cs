bcb70737978425d6ed61e16033fe5e96fb10e029
Gardel Alejandro Otero Verdugo <angrygaru@gmail.com>
2023-07-10 11:52:53 -0700

    Block all properties

--- rMakev2/Models/BlockElement.cs
+++ rMakev2/Models/BlockElement.cs
@@ -10,7 +10,32 @@
     public class Data
     {
         public string text { get; set; }
+        public int level { get; set; }
+        public string caption { get; set; }
+        public string alignment { get; set; }
+        public string url { get; set; }
+        public bool? withBorder { get; set; }
+        public bool? withBackground { get; set; }
+        public bool? stretched { get; set; }
+        public string style { get; set; }
+        public List<Item> items { get; set; }
+        public string link { get; set; }
+        public Meta meta { get; set; }
+        public string code { get; set; }
     }
+
+    public class Item
+    {
+        public string content { get; set; }
+        public List<object> items { get; set; }
+        public string text { get; set; }
+        public bool? @checked { get; set; }
+    }
+
+    public class Meta
+    {
+    }
+
 
     public class Root
     {
@@ -18,5 +43,7 @@
         public List<BlockElement> blocks { get; set; }
         public string version { get; set; }
     }
+
+
 
 }
